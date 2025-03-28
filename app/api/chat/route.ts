811446a3b67f320a10217f165debd0681557d20f
Sherin Grimbergen <sherin.grimbergen@cloudflight.io>
2025-03-28 09:33:38 +0100

    First draft is running

---
+++ app/api/chat/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { AzureOpenAI } from "openai";
+
+const openai = new AzureOpenAI({
+    apiKey: process.env.AZURE_OPENAI_API_KEY,
+    apiVersion: "2024-10-01-preview",
+    endpoint: process.env.AZURE_OPENAI_ENDPOINT,
+    deployment: process.env.AZURE_OPENAI_GPT_DEPLOYMENT
+});
+
+export async function POST(req: NextRequest) {
+    const { systemPrompt, messages } = await req.json();
+    console.log("Request started")
+
+    const response = await openai.chat.completions.create({
+        model: process.env.AZURE_OPENAI_GPT_DEPLOYMENT ?? "",
+        messages: [{ role: "system", content: systemPrompt }, ...messages],
+    });
+    console.log(response)
+    return NextResponse.json({ reply: response.choices[0].message.content });
+}
